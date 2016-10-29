dfef808faf3afe1665e8f5023be1af3abcecc4f7
SandraTobias-PC\Sandra Tobias <stobias@hawk.iit.edu>
2016-10-29 18:27:04 -0500

    I am trying to remove the title bar from the splash screen.  There is also an awkward pause at the beginning of the app now that I have tried to make it full screen.  The image is taking up the entire screen and there should be no status bar.  If you know how to fix these problems let me know so I can just implement your solution.  I'll edit the document and see if I can figure it out by later tonight.

--- app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/SplashScreen.java
+++ app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/SplashScreen.java
@@ -4,28 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         Thread myThread = new Thread(){
             @Override
             public void run() {
                 try {
-                    sleep(1000);
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
+                    sleep(10000);
                     finish();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
-
         myThread.start();
     }
 }
