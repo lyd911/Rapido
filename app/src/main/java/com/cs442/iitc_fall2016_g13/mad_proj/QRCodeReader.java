bcf17e4260b4c0a2fc6b78bb34f2a1e503267124
SandraTobias-PC\Sandra Tobias <stobias@hawk.iit.edu>
2016-10-29 21:02:21 -0500

    Added try-catch statement around the following statement: mCameraSource.start(mCameraView.getHolder());
    It is a permission check statement.

--- app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/QRCodeReader.java
+++ app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/QRCodeReader.java
@@ -1,6 +1,8 @@
 package com.cs442.iitc_fall2016_g13.mad_proj;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseArray;
@@ -22,14 +24,15 @@ public class QRCodeReader extends AppCompatActivity {
     private CameraSource mCameraSource;
     private SurfaceView mCameraView;
     private TextView mTVBarcodeInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode_reader);
 
 
-        mCameraView = (SurfaceView)findViewById(R.id.camera_view);
-        mTVBarcodeInfo = (TextView)findViewById(R.id.tv_QRCodeInfo);
+        mCameraView = (SurfaceView) findViewById(R.id.camera_view);
+        mTVBarcodeInfo = (TextView) findViewById(R.id.tv_QRCodeInfo);
 
         mBarcodeDetector =
                 new BarcodeDetector.Builder(this)
@@ -45,6 +48,16 @@ public class QRCodeReader extends AppCompatActivity {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
                 try {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
                     mCameraSource.start(mCameraView.getHolder());
                 } catch (IOException ie) {
                     Log.e("CAMERA SOURCE", ie.getMessage());
