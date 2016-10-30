cca1aa800692285e8a56b507c952ecaf160b8b5b
Augustus-19 <augustus_19@hotmail.com>
2016-10-29 19:47:07 -0500

    Adding QR Scanning and generating functionality

--- app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/MainActivity.java
+++ app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/MainActivity.java
@@ -1,13 +1,18 @@
 package com.cs442.iitc_fall2016_g13.mad_proj;
 
+import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text1, text2, text3, text4;
+    Button mBtnQRGenerate;
+    Button mBtnQRReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
         text2 = (TextView) findViewById(R.id.font_test_2);
         text3 = (TextView) findViewById(R.id.font_test_3);
         text4 = (TextView) findViewById(R.id.font_test_4);
+
+
+        mBtnQRGenerate = (Button) findViewById(R.id.btn_QRCodeGenerator);
+        mBtnQRReader = (Button) findViewById(R.id.btn_QRCodeReader);
+        mBtnQRGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), QRCodeGenerator.class));
+            }
+        });
+
+        mBtnQRReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), QRCodeReader.class));
+            }
+        });
+
 
         Typeface questrial_regular = Typeface.createFromAsset(getAssets(), "fonts/questrial_regular.ttf");
         Typeface quicksand_bold = Typeface.createFromAsset(getAssets(), "fonts/quicksand_bold.ttf");
---
+++ app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/QRCodeReader.java
@@ -0,0 +1,93 @@
+package com.cs442.iitc_fall2016_g13.mad_proj;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class QRCodeReader extends AppCompatActivity {
+
+
+    private BarcodeDetector mBarcodeDetector;
+    private CameraSource mCameraSource;
+    private SurfaceView mCameraView;
+    private TextView mTVBarcodeInfo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_reader);
+
+
+        mCameraView = (SurfaceView)findViewById(R.id.camera_view);
+        mTVBarcodeInfo = (TextView)findViewById(R.id.tv_QRCodeInfo);
+
+        mBarcodeDetector =
+                new BarcodeDetector.Builder(this)
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        mCameraSource = new CameraSource
+                .Builder(this, mBarcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        mCameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    mCameraSource.start(mCameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mCameraSource.stop();
+            }
+        });
+
+        mBarcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    mTVBarcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            mTVBarcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+
+    protected void onDestroy() {
+        super.onDestroy();
+        mCameraSource.release();
+        mBarcodeDetector.release();
+    }
+}
