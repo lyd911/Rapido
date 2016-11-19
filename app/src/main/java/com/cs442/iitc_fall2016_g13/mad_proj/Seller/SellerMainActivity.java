dc7d3d6547249404f3bc61e63697e638d2d7539f
yliu275 <lyd5650@gmail.com>
2016-11-19 01:00:43 -0600

    Refreshing every 10 seconds for seller to get pending orders added, but still need debugging

--- app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/Seller/SellerMainActivity.java
+++ app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/Seller/SellerMainActivity.java
@@ -1,8 +1,12 @@
 package com.cs442.iitc_fall2016_g13.mad_proj.Seller;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,7 +17,19 @@ import android.widget.TextView;
 import com.cs442.iitc_fall2016_g13.mad_proj.R;
 import com.cs442.iitc_fall2016_g13.mad_proj.ServerConnect.LoginActivity;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import android.os.Handler;
+
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 /**
  * Created by lyd on 2016/11/9.
@@ -22,11 +38,14 @@ import java.util.ArrayList;
 public class SellerMainActivity extends Activity{
 
     public static ArrayList<OneOrder> pendingOrders;
-    private ArrayAdapter<String> aa;
+    public static ArrayAdapter<String> aa;
+    public static Dialog dialog;
 
     public static ListView orders_listview;
     public static ArrayList<String> orderString;
     public static int currentOrder;
+   private int numberOfOrders;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +53,7 @@ public class SellerMainActivity extends Activity{
         setContentView(R.layout.seller_main_page);
         pendingOrders = new ArrayList<OneOrder>();
         orderString = new ArrayList<String>();
+
 
         TextView pending_textview=(TextView)findViewById(R.id.detail_textview);
         Button order_history_button=(Button)findViewById(R.id.order_history_button);
@@ -58,8 +78,6 @@ public class SellerMainActivity extends Activity{
             }
         });
 
-        new SellerGetPendingOrdersProcess(this).execute(LoginActivity.admin);
-
         orders_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -70,22 +88,147 @@ public class SellerMainActivity extends Activity{
                 startActivity(intent);
             }
         });
+
+        aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                SellerMainActivity.orderString);
+        orders_listview.setAdapter(aa);
+
+        callAsynchronousTask();
+
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        aa = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                SellerMainActivity.orderString);
-        orders_listview.setAdapter(aa);
         aa.notifyDataSetChanged();
     }
 
     public static void setOrderListView(ArrayAdapter aa){
         orders_listview.setAdapter(aa);
         aa.notifyDataSetChanged();
+    }
+
+
+    public void callAsynchronousTask() {
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+        TimerTask doAsynchronousTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                            doMysql();
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(doAsynchronousTask, 1, 10000); //execute in every 10000 ms
+    }
+
+
+    public void doMysql(){
+
+        AsyncTask<String,OneOrder,String> task = new AsyncTask<String, OneOrder, String>() {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+//                dialog.setMessage("Please wait");
+//                dialog.show();
+            }
+
+
+            @Override
+            protected String doInBackground(String... params) {
+                try{
+                    orderString.removeAll(orderString);
+                    pendingOrders.removeAll(pendingOrders);
+                    String admin = LoginActivity.admin;
+
+                    String link="http://rapido.counseltech.in/sellerRefresh.php";
+                    String data  = URLEncoder.encode("admin", "UTF-8") + "=" + URLEncoder.encode(admin, "UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write( data );
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    while((line = reader.readLine()) != null)
+                    {
+                        sb.append(line);
+                        break;
+                    }
+                    String rawdata=sb.toString();
+
+                    int i=0;
+                    String s1[] = rawdata.split("\\}");
+                    if(s1!=null){
+                        for(i=0;i<s1.length-1;i++){
+                            String s2[]=s1[i].split("\"");
+                            OneOrder oneOrder = new OneOrder();
+                            oneOrder.setOrder_id(s2[3]);
+                            oneOrder.setRest_id(s2[7]);
+                            oneOrder.setCust_id(s2[11]);
+                            oneOrder.setMenu_list(s2[15]);
+                            oneOrder.setStatus(s2[19]);
+
+                            SellerMainActivity.pendingOrders.add(i,oneOrder);
+                        }}
+
+                    if(pendingOrders !=null){
+                        numberOfOrders = pendingOrders.size();
+                        for(i=0;i<numberOfOrders;i++){
+                            String Status;
+                            if(pendingOrders.get(i).getStatus().equals("0")){
+                                Status = "Status: Not Started";
+                            }
+                            else if(pendingOrders.get(i).getStatus().equals("1")){
+                                Status = "Status: Cooking";
+                            }
+                            else Status = "Status: Finished Cooking";
+                            String ss= "Order ID: " + pendingOrders.get(i).getOrder_id()+"\n"+
+                                    "Customer ID: " + pendingOrders.get(i).getCust_id()+"\n"+
+                                    "Menu List: " + pendingOrders.get(i).getMenu_list()+"\n"+
+                                    Status;
+
+                            orderString.add(ss);
+                        }}
+
+
+                    aa.notifyDataSetChanged();
+
+                    return null;
+                }catch (Exception e){
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+            }
+
+
+
+        };
+        if(Build.VERSION.SDK_INT >= 11)
+            task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        else
+            task.execute();
     }
 }
