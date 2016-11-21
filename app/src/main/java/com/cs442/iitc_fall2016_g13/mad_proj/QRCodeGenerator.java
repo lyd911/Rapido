18e0b1a2623d5add737269f4f263984f88610bf6
Augustus-19 <augustus_19@hotmail.com>
2016-11-21 11:30:10 -0600

    Adding back files

---
+++ app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/QRCodeGenerator.java
@@ -0,0 +1,61 @@
+package com.cs442.iitc_fall2016_g13.mad_proj;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class QRCodeGenerator extends Activity {
+
+
+    private Button mGenerateButton;
+    private EditText mEditTextQRCodeInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_generator);
+
+        Intent getData = getIntent();
+        String billNumber = getData.getStringExtra("BILL#");
+
+        final Context context = this;
+        mEditTextQRCodeInput = (EditText) this.findViewById(R.id.et_GenerateQR);
+
+
+        mEditTextQRCodeInput.setText(billNumber);
+
+        mGenerateButton = (Button) this.findViewById(R.id.btn_QRCodeGenerate);
+        mGenerateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text2Qr = mEditTextQRCodeInput.getText().toString();
+                if(text2Qr.length() > 0){
+
+                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                    try {
+                        BitMatrix bitMatrix = multiFormatWriter.encode(text2Qr, BarcodeFormat.QR_CODE,200,200);
+                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                        Intent intent = new Intent(context, QRActivity.class);
+                        intent.putExtra("pic",bitmap);
+                        context.startActivity(intent);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/cs442/iitc_fall2016_g13/mad_proj/ServerConnect/CustomerPlaceOrderProcess.java
@@ -0,0 +1,105 @@
+package com.cs442.iitc_fall2016_g13.mad_proj.ServerConnect;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cs442.iitc_fall2016_g13.mad_proj.Map_distance.MapsActivity;
+import com.cs442.iitc_fall2016_g13.mad_proj.NearbyPlaces;
+import com.cs442.iitc_fall2016_g13.mad_proj.fragmentlayout.billingactivity;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+/**
+ * Created by lyd on 2016/11/19.
+ */
+
+public class CustomerPlaceOrderProcess extends AsyncTask<String,Void,String> {
+
+    private TextView statusField,roleField;
+    private Context context;
+    public static ProgressDialog dialog ;
+
+
+    //flag 0 means get and 1 means post.(By default it is get.)
+    public CustomerPlaceOrderProcess(Context context) {
+        this.context = context;
+        dialog = new ProgressDialog(context);
+
+    }
+
+    protected void onPreExecute(){
+        dialog.setMessage("Please wait");
+        dialog.show();
+
+    }
+
+
+    @Override
+    protected String doInBackground(String... arg0) {
+
+        try{
+            String rest_id = (String)arg0[0];
+            String cust_id=(String)arg0[1];
+            String menu_list=(String)arg0[2];
+
+
+            String link="http://rapido.counseltech.in/CustomerPlaceOrder.php";
+            String data  = URLEncoder.encode("rest_id", "UTF-8") + "=" + URLEncoder.encode(rest_id, "UTF-8");
+            data += "&" + URLEncoder.encode("cust_id", "UTF-8") + "=" + URLEncoder.encode(cust_id, "UTF-8");
+            data += "&" + URLEncoder.encode("menu_list", "UTF-8") + "=" + URLEncoder.encode(menu_list, "UTF-8");
+
+
+            URL url = new URL(link);
+            URLConnection conn = url.openConnection();
+
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+            wr.write( data );
+            wr.flush();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                sb.append(line);
+                break;
+            }
+            Intent intent = new Intent(context, NearbyPlaces.class);
+            context.startActivity(intent);
+
+            Intent i = new Intent(context, MapsActivity.class);
+            context.startActivity(i);
+            return sb.toString();
+        }
+        catch(Exception e){
+            return new String("Exception: " + e.getMessage());
+
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+
+        Toast.makeText(getApplicationContext(),"Order placed!",Toast.LENGTH_LONG).show();
+
+        dialog.dismiss();
+
+
+    }
+}
